8ec0c90617a45af4a8024ae12cf7b3b73cbdd28b
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,19 @@
-#######################################
-# modules/puppet/manifests/init.pp - manage puppet stuff
+#
 # puppet module
+# modules/puppet/manifests/init.pp - manage puppet stuff
 # original by luke kanies
 # http://github.com/lak
-# adaapted by puzzel itc
-# merged with immerda project groups
-# solution
-#######################################
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "puppet": }
 
