377758ee13fd96364265c182c6469a6697c80282
Marcel Haerry <haerry@puzzle.ch>
2009-04-06 11:13:37 +0200

    include cron module

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,6 +1,7 @@
 # manifests/cron.pp
 
 class puppet::cron inherits puppet {
+    include cron
     case $kernel {
         linux: { include puppet::cron::linux }
         openbsd: { include puppet::cron::openbsd }
