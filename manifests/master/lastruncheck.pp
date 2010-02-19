694b3fdaea7850fe778f731ed0e1f96911e66ee6
Simon Josi <josi@puzzle.ch>
2010-02-19 20:09:50 +0100

    remove dependecy

--- manifests/master/lastruncheck.pp
+++ manifests/master/lastruncheck.pp
@@ -14,7 +14,6 @@ class puppet::master::lastruncheck {
     include puppet::master
     file{'/etc/cron.d/puppetlast.cron':
         content => "40 10,22 * * * root /usr/share/puppet/ext/puppetlast\n",
-        require => File['/opt/bin/puppetlast'],
         owner => root, group => 0, mode => 0644;
     }
 }
