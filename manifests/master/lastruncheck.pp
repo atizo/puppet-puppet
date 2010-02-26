6b3847211307b5cbb330c04f62013af4433d56cc
Simon Josi <josi@puzzle.ch>
2010-02-26 16:59:25 +0100

    lastruncheck script must be executable

--- manifests/master/lastruncheck.pp
+++ manifests/master/lastruncheck.pp
@@ -14,7 +14,7 @@ class puppet::master::lastruncheck {
     include puppet::master
     file{'/usr/local/sbin/lastruncheck':
         source => "puppet://$server/modules/puppet/master/lastruncheck",
-        owner => root, group => 0, mode => 0644;
+        owner => root, group => 0, mode => 0755;
     }
     file{'/etc/cron.d/puppetlast.cron':
         content => "40 10,22 * * * root /usr/local/sbin/lastruncheck\n",
