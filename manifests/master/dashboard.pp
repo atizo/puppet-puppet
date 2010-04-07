2b033a8fe1fbf000096e88165c58135d814cb936
Simon Josi <josi@puzzle.ch>
2010-04-07 14:48:30 +0200

    migrate to dashboard rpm

--- manifests/master/dashboard.pp
+++ manifests/master/dashboard.pp
@@ -14,32 +14,38 @@ define puppet::master::dashboard(
 
     $use_dashboard = true
 
-    git::clone{'puppet-dashboard':
-        git_repo => 'git://github.com/reductivelabs/puppet-dashboard.git',
-        projectroot => '/opt/puppet-dashboard',
-        notify => Exec['rake install'],
+    yum::managed_yumrepo{'puppetlabs':
+        descr => 'Puppet Labs Packages',
+        baseurl => "http://yum.puppetlabs.com/base/",
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => "http://yum.puppetlabs.com/RPM-GPG-KEY-reductive"
+        priority => 1,
     }
-    exec{'rake install':
-        cwd => $root,
-        refreshonly => true,
+    package{'puppet-dashboard'
+        ensure => installed,
     }
-    file{"$root/config/database.yml":
-        content => template('modules/puppet/dashboard/database.yml'),
-        require => Git::Clone['puppet-dashboard'],
-    }
-    file{'/usr/lib/ruby/site_ruby/1.8/puppet/reports/puppet_dashboard.rb':
-        source => '/opt/puppet-dashboard/lib/puppet/puppet_dashboard.rb',
-        require => Git::Clone['puppet-dashboard'],
-    }
-    service{'dashboard-webrick':
-        provider => base,
-        ensure => running,
-        pattern => "ruby $root/script/server -d",
-        start => "$root/script/server -d",
-        stop => "kill -9 `ps x | grep 'ruby $root/script/server -d' | grep -v grep | awk '{print $1}'`",
-        require => [
-            File["$root/config/database.yml"],
-            File['/usr/lib/ruby/site_ruby/1.8/puppet/reports/puppet_dashboard.rb'],
-        ]
-    }
+#    exec{'rake install':
+#        cwd => $root,
+#        refreshonly => true,
+#    }
+#    file{"$root/config/database.yml":
+#        content => template('modules/puppet/dashboard/database.yml'),
+#        require => Package['puppet-dashboard'],
+#    }
+#    file{'/usr/lib/ruby/site_ruby/1.8/puppet/reports/puppet_dashboard.rb':
+#        source => '/opt/puppet-dashboard/lib/puppet/puppet_dashboard.rb',
+#        require => Package['puppet-dashboard'],
+#    }
+#    service{'dashboard-webrick':
+#        provider => base,
+#        ensure => running,
+#        pattern => "ruby $root/script/server -d",
+#        start => "$root/script/server -d",
+#        stop => "kill -9 `ps x | grep 'ruby $root/script/server -d' | grep -v grep | awk '{print $1}'`",
+#        require => [
+#            File["$root/config/database.yml"],
+#            File['/usr/lib/ruby/site_ruby/1.8/puppet/reports/puppet_dashboard.rb'],
+#        ]
+#    }
 }
