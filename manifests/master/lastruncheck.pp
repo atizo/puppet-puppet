13438098e7bd1d6f4f212174da22a1d42a9d4b9f
Marcel Haerry <haerry@puzzle.ch>
2010-11-08 15:46:04 +0100

    add new lastruncheck script

--- manifests/master/lastruncheck.pp
+++ manifests/master/lastruncheck.pp
@@ -16,8 +16,21 @@ class puppet::master::lastruncheck {
     source => "puppet:///modules/puppet/master/lastruncheck",
     owner => root, group => 0, mode => 0755;
   }
+
+  $puppet_lastruncheck_ignorehosts_str = $puppet_lastruncheck_ignorehosts ? {
+    '' => '',
+    undef => '',
+    default => "--ignore-hosts ${puppet_lastruncheck_ignorehosts}"
+  }
+
+  $puppet_lastruncheck_timeout_str = $puppet_lastruncheck_timeout ? {
+    '' => '',
+    undef => '',
+    default => "--timeout ${puppet_lastruncheck_timeout}"
+  }
+
   file{'/etc/cron.d/puppetlast.cron':
-    content => "40 10,22 * * * root /usr/local/sbin/lastruncheck\n",
+    content => "40 10,22 * * * root /usr/local/sbin/lastruncheck ${puppet_lastruncheck_timeout_str} ${puppet_lastruncheck_ignorehosts_str} ${$puppet_lastruncheck_additionaloptions}\n",
     owner => root, group => 0, mode => 0644;
   }
 }
